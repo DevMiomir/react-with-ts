997ab896efceb383563dc3aecc9082ce09fabc59
devMiomir <manasijevic.miomir@gmail.com>
2021-09-09 13:22:06 +0200

    added files to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List'
 
-function App() {
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
+function App() {  
+ const [people, setPeople] = useState<IState["people"]>([
+   {
+     name: "Pera Peric",
+     url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+     age: 33,
+     note: "Great dude"
+   }
+ ])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Peeps invited to React party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { IState as IProps } from '../App'
+
+
+const list: React.FC<IProps> = ({people}) => {
+
+    const renderList = (): JSX.Element[] => {
+        return people.map((person, index) => {
+            return (
+                <li className="List" key={index}>
+                    <div className="List-header">
+                        <img className="List-img" src={person.url} alt="info" />
+                        <h2>{person.name}</h2>
+                    </div>
+                    <p>{person.age} years old</p>
+                    <p className="List-note">{person.note}</p>
+                </li>
+            )
+        })
+    }
+
+    return (
+        <ul>
+           {renderList()}
+        </ul>
+    )
+};
+
+export default list;
